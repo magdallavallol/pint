0644b556a666050127fbbd714701ddf7a8d0a5e4
Magda Llavallol <magdallavallol@gmail.com>
2014-08-16 13:34:29 -0300

    Installed Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pinteresting::Application.routes.draw do
+  
+  devise_for :users
  root "pages#home"
  get "about"=>"pages#about"
  
@@ -56,4 +58,5 @@ Pinteresting::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+ # devise_for :users 
 end
